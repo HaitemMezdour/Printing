c790c1461d39b29bd7d14f2e2c94e4a3806e75c0
Haithem Mezdour <h.mezdour@intelliway.fr>
2021-05-18 09:45:37 +0200

    nothig realy just spaces and tabs

--- Printing/ESCcommands.cs
+++ Printing/ESCcommands.cs
@@ -9,17 +9,11 @@ namespace Printing
 {
     class ESC
     {
-     
-
-
+ 
             public readonly string GotoStoCutPosition = "\x1c\x28\x4c\x02\x00\x42\x30";
             public readonly string GotoStartPosition = "\x1c\x28\x4c\x02\x00\x43\x32";
             public readonly string InisialisePrinter = "\x1B\x40";
 
 
-
-      
-
-
-}
+     }
 }
